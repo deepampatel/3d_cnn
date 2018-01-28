7711f7a16ae673f0924d7dd407cd7deb6502444a
Deepam Patel <deepam1803@yahoo.in>
2018-01-28 10:14:00 +0530

    Added activation functions

--- model.py
+++ model.py
@@ -9,11 +9,11 @@ from keras.utils import to_categorical
 def create_model():
     inp = Input(shape=(32, 32, 32, 4))
 
-    Conv_3d_1 = Conv3D( filters=4, kernel_size = (6, 6, 6), strides = (1, 1, 1), padding = 'valid', activation = None, use_bias = True)(inp)
+    Conv_3d_1 = Conv3D( filters=4, kernel_size = (6, 6, 6), strides = (1, 1, 1), padding = 'valid', activation = 'relu', use_bias = True)(inp)
 
     max_pool_1 = MaxPooling3D( pool_size = (2, 2, 2), strides = (1, 1, 1), padding = 'valid')(Conv_3d_1)
 
-    Conv_3d_2 = Conv3D(filters = 4, kernel_size = (4, 4, 4), strides = (1, 1, 1), padding = 'valid', activation = None, use_bias = True)(max_pool_1)
+    Conv_3d_2 = Conv3D(filters = 4, kernel_size = (4, 4, 4), strides = (1, 1, 1), padding = 'valid', activation = 'relu', use_bias = True)(max_pool_1)
 
     max_pool_2 = MaxPooling3D( pool_size = (2, 2, 2), strides = (1, 1, 1), padding = 'valid')(Conv_3d_2)
     flatten1 = Flatten()(max_pool_2)
