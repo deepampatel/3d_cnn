d115b1bc958d9e1fec98b878dcdf4989dee62946
Deepam Patel <deepam1803@yahoo.in>
2018-01-28 10:32:41 +0530

    updated model architecture

--- model.py
+++ model.py
@@ -9,14 +9,25 @@ from keras.utils import to_categorical
 def create_model():
     inp = Input(shape=(32, 32, 32, 4))
 
-    Conv_3d_1 = Conv3D( filters=4, kernel_size = (6, 6, 6), strides = (1, 1, 1), padding = 'valid', activation = 'relu', use_bias = True)(inp)
+    Conv_3d_1 = Conv3D( filters=6, kernel_size = (6, 6, 6), strides = (1, 1, 1), padding = 'valid',
+                        activation = 'relu', use_bias = True)(inp)
 
     max_pool_1 = MaxPooling3D( pool_size = (2, 2, 2), strides = (1, 1, 1), padding = 'valid')(Conv_3d_1)
 
-    Conv_3d_2 = Conv3D(filters = 4, kernel_size = (4, 4, 4), strides = (1, 1, 1), padding = 'valid', activation = 'relu', use_bias = True)(max_pool_1)
+    Conv_3d_2 = Conv3D(filters = 3, kernel_size = (4, 4, 4), strides = (1, 1, 1), padding = 'valid',
+                       activation = 'relu', use_bias = True)(max_pool_1)
 
     max_pool_2 = MaxPooling3D( pool_size = (2, 2, 2), strides = (1, 1, 1), padding = 'valid')(Conv_3d_2)
-    flatten1 = Flatten()(max_pool_2)
+
+    Conv_3d_3 = Conv3D(filters=2, kernel_size=(4, 4, 4), strides=(1, 1, 1), padding='valid',
+                       activation='relu', use_bias=True)(max_pool_2)
+
+    max_pool_3 = MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1), padding='valid')(Conv_3d_3)
+
+    flatten1 = Flatten()(max_pool_3)
+
     out = Dense(2, activation='sigmoid')(flatten1)
+
     model = Model(inputs=inp, outputs=out)
+    
     return model
